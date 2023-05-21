04664ce8066f6194788f448670ad22f8dda4243c
sara9 <sara9@MSISARA>
2023-05-21 17:55:10 +0200

    user 27 analysis started

--- queries.sql,.sql
+++ queries.sql,.sql
@@ -1,4 +1,4 @@
 select person_id, min(time_stamp) as min,
  max(time_stamp) as max, max(time_stamp) - min(time_stamp) as duration
 from example_trajectory_students
-group by person_id
+group by person_idcreate table join_table as (	select u._uid_, u.geom, u.id, u.person_id, u.time_stamp::timestamp, u.x, u.y	from user_27 as u join proximity_27 as p on u.id = p.id)
